eeeba1f3ae09136d425962a94931379bf5604c90
Adam Hall <adam.hall@aligent.com.au>
2021-06-30 10:35:06 +0930

    Pull functions out to individual constructs and add WIP readme

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-import { PrerenderLambdaStack } from "./lib/lambda-stack";
+import { PrerenderLambdaStack } from "./lib/prerender-lambda-stack";
+import { PrerenderFunction } from "./lib/prerender-construct";
+import { PrerenderCheckFunction } from "./lib/prerender-check-construct";
 
-export { PrerenderLambdaStack };
+export { PrerenderLambdaStack, PrerenderFunction, PrerenderCheckFunction };
